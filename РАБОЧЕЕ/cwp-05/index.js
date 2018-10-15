514d8f728337d012edfbe410fcba24696e70fc19
anna <anna_savenko98@mail.ru>
2018-10-15 11:17:19 +0300

    v1

---
+++ РАБОЧЕЕ/cwp-05/index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+const server = http.createServer((req, res) => {
+    parseBodyJson(req, (err,payload) =>{
+        const c = {c: payload.a + payload.b};
+
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'application/json');
+        res.end(JSON.stringify(c));
+    });
+});
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+function  parseBodyJson(req,cb) {
+    let body = [];
+
+    req.on('data', function(chunk){
+        body.push(chunk);
+    }).on('end', function () {
+        body = Buffer.concat(body).toString();
+        let params = JSON.parse(body);
+        cb(null,params);
+    });
+}
+
+
